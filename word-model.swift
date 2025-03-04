8a7fca17efdf91167aa0fd3cde90d073a9fbc40f
AlephArcade <shlomofellig@gmail.com>
2025-03-04 09:47:50 -0500

    Update word-model.swift

--- word-model.swift
+++ word-model.swift
@@ -1,8 +1,15 @@
 import Foundation
 
 struct Word: Identifiable, Codable {
-    let id = UUID()
+    let id: UUID
     let hebrew: String
     let transliteration: String
     let meaning: String
+    
+    init(id: UUID = UUID(), hebrew: String, transliteration: String, meaning: String) {
+        self.id = id
+        self.hebrew = hebrew
+        self.transliteration = transliteration
+        self.meaning = meaning
+    }
 }
